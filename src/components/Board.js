cb39d871c42287d941c99497bbb1b8d709742113
lowlyphe <jones.kyle2893@gmail.com>
2022-08-26 18:11:18 -0500

    working on prop mapping for todo

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,21 +1,22 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import Todo from './Todo.js';
 import Doing from './Doing.js';
 import Done from './Done.js';
 
 export default function Board({ currentBoardId }) {
-    const tasks = []
-    const todo = []
-    const doing = []
-    const done = []
+    const [todo, setTodo] = useState([])
+    const [doing, setDoing] = useState([])
+    const [done, setDone] = useState([])
+
 
     useEffect(() => {
         axios.get(`http://localhost:3001/api/tasks/${currentBoardId || 1}`).then(res => {
+            console.log(res.data)
           for (let i =0; i < res.data.length; i++) {
-            if (res.data[i].status === "Todo") todo.push(res.data[i]);
-            else if (res.data[i].status === "Doing") doing.push(res.data[i]);
-            else if (res.data[i].status === "Done") done.push(res.data[i]);
+            if (res.data[i].status === "Todo") setTodo([...todo, res.data[i]]);
+            else if (res.data[i].status === "Doing") setDoing([...doing, res.data[i]]);
+            else if (res.data[i].status === "Done") setDone([...done, res.data[i]]);
           }
         })
       }, []);
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Todo({ todo }) {
     console.log("in todos", todo)
   return (
-    <div>
-        {todo.map(todo => <div>{todo.taskName}</div>)}
+    <div className='flex flex-col border border-solid-2 border-black'>
+        {todo.map(todo => <div key={todo.taskname}>{todo.taskname}</div>)}
     </div>
   )
 }
